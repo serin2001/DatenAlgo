01ff3957b6cdff72fc81fe88646d8188ae83d68e
andreas <andreas@ubuntu-7B79>
2025-05-09 13:35:38 +0200

    Blatt 5 - A2 - LOG Laufzeit + Pseudocode

--- src/ex52/ClosestPair.java
+++ src/ex52/ClosestPair.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class ClosestPair {
 
 	public static void main(String[] args) {
-		int n = 15;
+		int n = 12;
 		int[] h = new int[n];
 		h = new Random().ints(n, 0, 101).toArray(); // Generiere n ganze Zahlen aus [0, 100]
 
@@ -15,7 +15,7 @@ public class ClosestPair {
 		s = new Random().ints(m, 0, 101).toArray(); // Generiere m ganze Zahlen aus [0, 100]
 		Arrays.sort(s); // sortiere
 
-		System.out.println("Häuser: " + Arrays.toString(h));
+		System.out.println("Haeuser: " + Arrays.toString(h));
 		System.out.println("Sirenen: " + Arrays.toString(s));
 
 		int[] d = new int[n];
@@ -32,15 +32,11 @@ public class ClosestPair {
 			return array[a];
 		else {
 			int mi = (a + b) / 2;
-			// System.out.println("Mitte " + mi);
-			int l = findClosest(array, a, mi, x);
-			// System.out.println("links " + l);
-			int r = findClosest(array, mi + 1, b, x);
-			// System.out.println("Rechts " + r);
-			if (Math.abs(l - x) <= Math.abs(r - x))
-				return l;
-			else
-				return r;
+			if (x <= array[mi]) { // wenn x im linken Teilarray liegt, soll nur dort gesucht werden
+				return findClosest(array, a, mi, x);
+			} else { // wenn x im rechten Teilarray liegt, soll nur dort gesucht werden
+				return findClosest(array, mi + 1, b, x);
+			}
 		}
 	}
 
